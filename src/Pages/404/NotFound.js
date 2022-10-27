8d42317d054875743b2cca97ef0ea5879a6194bd
pulok-thedeveloper <mdpulokhasan@gmail.com>
2022-10-27 23:58:52 +0600

    third commit

--- src/Pages/404/NotFound.js
+++ src/Pages/404/NotFound.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div>
-            
+        <div className='text-center'>
+            <h1>404 Not Found</h1>
         </div>
     );
 };
